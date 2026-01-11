591f6205cff020b885104b393db6cb7cb58a0704
tomwebb644 <tomwebb64@hotmail.co.uk>
2026-01-11 18:55:11 +0000

    Add multiplayer pause, restart, and countdown

--- server/server.js
+++ server/server.js
@@ -87,6 +87,13 @@ wss.on("connection", (ws) => {
       return;
     }
 
+    if (msg.type === "control") {
+      if (room.host && room.host.readyState === 1) {
+        safeSend(room.host, { type: "control", from: ws.player, action: msg.action });
+      }
+      return;
+    }
+
     // Host migration: if p1 leaves, allow p2 to become host
     if (msg.type === "claim_host") {
       if (!room.host || room.host.readyState !== 1) {
